62bfeaad18e9dcc9c434de8227f849270aa5033e
Bhavan Bagri <bhavanbagri@dhcp-v109-205.mobile.uci.edu>
2018-08-21 22:04:18 -0700

    finished PyBank

---
+++ PyPoll/main.py
@@ -0,0 +1,63 @@
+import os
+import csv
+
+csvpath = os.path.join("..", "Resources /", "election_data.csv")
+outputfile = os.path.join("..", "Resources /", "Election——Results.txt")
+
+total_votes = 0
+
+candidates = []
+candidates_votecount = {}
+
+winner = ""
+winner_votecount = 0
+
+with open(csvpath) as rawdata:
+    reader = csv.reader(rawdata)
+    header = next(reader)
+
+    for row in reader:
+
+        total_votes += 1
+        
+        candidates_name = row[2]
+
+        if candidates_name not in candidates:
+
+            candidates.append(candidates_name)
+            candidates_votecount[candidates_name] = 0
+
+        candidates_votecount[candidates_name] += 1
+
+with open(outputfile, "w") as txt_file:
+
+    election_results = (
+        f"\n\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+
+    txt_file.write(election_results)
+
+    for x in candidates_votecount:
+
+        votes = candidates_votecount.get(x)
+        percenatge_count = float(votes) / float(total_votes) * 100
+
+        if (votes > winner_votecount):
+            winner_votecount = votes
+            winner = x
+
+        listofresults = f"{x}: {percenatge_count:.3f}% ({votes})\n"
+        print(listofresults, end="")
+
+        txt_file.write(listofresults)
+
+    election_results_part2 = (
+        f"-------------------------\n"
+        f"Winner: {winner}\n"
+        f"-------------------------\n")
+    print(election_results_part2)
+
+    txt_file.write(election_results_part2)
