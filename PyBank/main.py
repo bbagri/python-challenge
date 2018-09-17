85af23a39a01e5df984e0059a2397862fbc7c803
Bhavan Bagri <bhavanbagri@bhavans-MacBook-Pro.local>
2018-09-17 16:52:52 -0700

    redobank

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,75 +4,56 @@ import csv
 csvpath = os.path.join("..", "Resources /", "budget_data.csv")
 outputfile = os.path.join("..", "Resources /", "Financial Analysis.txt")
 
-total_months = 0
-total = 0
-greatest_increase = ["", 0]
-greatest_decrease = ["", 1000000000000]
+with open(csvpath, 'r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    csv_header = next(csv_reader)
+    print(f"CSV Header: {csv_header}")
 
-month_of_change = []
-net_change_list = []
+    month_count = 0
+    total_profit = 0
+    profit_inc = ["", 0]
+    loss_dec = ["", 8897553]
+    change_m = []
 
+    row = next(csv_reader)
+    previous_value = int(row[1])
+    total_profit = total_profit + int(row[1])
 
-with open(csvpath) as rawdata:
-    reader = csv.reader(rawdata)
-    header = next(reader)
+    for row in csv_reader:
+        month_count += 1
+        total_profit += int(row[1])
+        #change_value = int(row[1]) - previous_value
+        change_value = total_profit - previous_value
+        previous_value = int(row[1])
+        change_m = change_m + [row[0]]
 
-    first_row = next(reader)
-    total_months = total_months + 1
-    total = total + int(first_row[1])
-    prev_net = int(first_row[1])
+        if change_value >= profit_inc[1]:
+            profit_inc[0] = row[0]
+            profit_inc[1] = change_value
 
-    for row in reader:
-        total_months = total_months + 1
-        total = total + int(row[1])
+        elif change_value <= loss_dec[1]:
+            loss_dec[0] = row[0]
+            loss_dec[1] = change_value
 
-        net_change = int(row[1]) - prev_net
-        prev_net = int(row[1])
-        net_change_list = net_change_list + [net_change]
-        month_of_change = month_of_change + [row[0]]
+avg = str(round(change_value / (month_count-1),2))
 
-        if net_change > greatest_increase[1]:
-            greatest_increase[0] = row[0]
-            greatest_increase[1] = net_change
+print("Financial Analysis")
+print("---------------------")
+print("Total Months: " + str(month_count))
+print("Total Revenue: $" + str(total_profit))
+print("Average Change: $" + str(avg))
+print("Greatest Increase in Profits: " + profit_inc[0] + " ($" + str(profit_inc[1]) + ")")
+print("Greatest Decrease in Profits: " + loss_dec[0] + " ($" + str(loss_dec[1]) + ")")
 
-        if net_change < greatest_decrease[1]:
-            greatest_decrease[0] = row[0]
-            greatest_decrease[1] = net_change
-
-net_monthly_avg = sum(net_change_list) / len(net_change_list)
-
-report = (
-    f"\nFinancial Analysis\n"
-    f"----------------------------\n"
-    f"Total Months: {total_months}\n"
-    f"Total: ${total}\n"
-    f"Average  Change: ${net_monthly_avg:.2f}\n"
-    f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n"
-    f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
-
-print(report)
-
-with open(outputfile, "w") as txt_file:
-    txt_file.write(report)
-
-
-# Specify the file to write to
 output_path = os.path.join("output", "PyBankAns.csv")
 
-# Open the file using "write" mode. Specify the variable to hold the contents
 with open(output_path, 'w', newline='') as csvfile:
 
-   # Initialize csv.writer
    csvwriter = csv.writer(csvfile, delimiter=',')
 
-   # Write the first row (column headers)
-   csvwriter.writerow(['Financial Analysis'])
-
-  # Write the second row
-   csvwriter.writerow(['----------------------------',])
-
-   csvwriter.writerow(['Total Months : '+str(total_months)])
-   csvwriter.writerow(['Total : ' +str(total)])
-   csvwriter.writerow(['Average Change : '+str(net_monthly_avg)])
-   csvwriter.writerow(['Greatest Increase in Profits : ' +str(greatest_increase)])
-   csvwriter.writerow(['Greatest Decrease in Profits : ' +str(greatest_decrease)])
+   csvwriter.writerow(['Financial Analysis-----------------',])
+   csvwriter.writerow(['Total Months : '+str(month_count)])
+   csvwriter.writerow(['Total : ' +str(total_profit)])
+   csvwriter.writerow(['Average Change : '+str(avg)])
+   csvwriter.writerow(['Greatest Increase in Profits : ' +str(profit_inc)])
+   csvwriter.writerow(['Greatest Decrease in Profits : ' +str(loss_dec)])
